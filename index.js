28c5b8d4c2ae9c3009fa1915f403da3889c32095
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-12 05:33:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ function moveDodgerLeft() {
     }
 }
 
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
 
+    if (left < 360 ) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
 
 
 document.addEventListener('keydown', function(e) {
