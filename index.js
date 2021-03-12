e5f1a245a8dab94aedc2c453b22e0b99cc6c04b3
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-12 05:32:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+
+
+
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
